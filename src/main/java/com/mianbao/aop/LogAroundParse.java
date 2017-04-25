34977276da86bc506c43d8b9e2154883de8edeec
zoujiajian <zoujiajian@lianjia.com>
2017-04-26 03:10:06 +0800

    aop

--- src/main/java/com/mianbao/aop/LogAroundParse.java
+++ src/main/java/com/mianbao/aop/LogAroundParse.java
@@ -1,12 +1,13 @@
 package com.mianbao.aop;
 
-
 import com.alibaba.fastjson.JSON;
-import com.mianbao.annotation.LogAround;
+import com.mianbao.common.Result;
+import com.mianbao.pojo.LogAroundPojo;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -14,8 +15,7 @@ import org.springframework.stereotype.Component;
 import java.lang.reflect.Method;
 
 /**
- * Created by zoujiajian on 2017-3-9.
- * 环绕日志解析
+ * Created by zoujiajian on 2017-4-24.
  */
 @Aspect
 @Component
@@ -23,36 +23,43 @@ public class LogAroundParse {
 
     private static final Logger logger = LoggerFactory.getLogger(LogAroundParse.class);
 
-    @Pointcut("execution(* com.mianbao.controller..*(..))")
+    @Pointcut("execution(* com.minabao.controller..*(..))")
     public void pointCut(){}
 
     @Around("pointCut()")
     public Object advice(ProceedingJoinPoint joinPoint){
         Object target = joinPoint.getTarget();
-        String methodName = joinPoint.getSignature().getName();
-        Class clazz = target.getClass();
-        Method[] methods = clazz.getDeclaredMethods();
+        MethodSignature methodSignature = (MethodSignature)joinPoint.getSignature();
+        Method currentMethod = methodSignature.getMethod();
+        String methodName = currentMethod.getName();
+        String className = target.getClass().getSimpleName();
+
         Object result = null;
+        long startTime = System.currentTimeMillis();
         try {
-            long startTime = System.currentTimeMillis();
             result = joinPoint.proceed();
             long endTime = System.currentTimeMillis();
-            for(Method method : methods){
-                if(method.getName().equals(methodName)){
-                    //判断拦截方法上是否存在logAround日志注解
-                    if(method.isAnnotationPresent(LogAround.class)){
-                        Object[] arguments = joinPoint.getArgs();
+            if(currentMethod.isAnnotationPresent(com.mianbao.annotation.LogAround.class)){
+                Result methodResult = (Result) result;
+                Object[] arguments = joinPoint.getArgs();
 
-                        com.mianbao.pojo.LogAround logAround = new com.mianbao.pojo.LogAround();
-                        logAround.setRequest(arguments);
-                        logAround.setResponse(result);
-                        logAround.setTimeOut(endTime - startTime);
-                        logger.info(JSON.toJSONString(logAround));
-                    }
-                }
+                LogAroundPojo logAround = new LogAroundPojo();
+                logAround.setClassName(className);
+                logAround.setMethodName(methodName);
+                logAround.setRequest(arguments);
+                logAround.setResponse(JSON.toJSONString(result));
+                logAround.setTimeOut(endTime - startTime);
+                logAround.setSuccess(methodResult.isSuccess());
+                logger.info("monitor log info: " + logAround.toString());
             }
         } catch (Throwable throwable) {
-            throwable.printStackTrace();
+            logger.error("", throwable);
+        }finally {
+            long elapseTime = System.currentTimeMillis() - startTime;
+            String monitorMethodName = className + "." + methodName;
+            if(elapseTime > 1000){
+                logger.warn(String.format("controller %s elapse %s", monitorMethodName, elapseTime));
+            }
         }
         return result;
     }
